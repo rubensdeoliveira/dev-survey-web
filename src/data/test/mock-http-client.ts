0813d0a4d866cb8a0abb76a91df5180945bf5326
Jose Rubens de Oliveira Junior <rubensojunior6@gmail.com>
2021-06-19 23:04:57 -0300

    feat: ensure auth receive body correct

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -5,9 +5,11 @@ import {
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string
+  body?: Record<string, unknown>
 
   async post(params: HttpPostParams): Promise<void> {
     this.url = params.url
+    this.body = params.body
     return Promise.resolve()
   }
 }
